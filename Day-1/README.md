d844f9e817ddd8b329356647590a4ae77b6b358b
Ashutosh Singh <singhashutosh4123@gmail.com>
2026-01-13 20:17:45 +0530

    Day 1 files

---
+++ Day-1/README.md
@@ -0,0 +1,2 @@
+# Python-100-Days-100-Projects
+brush up my python fundamentals and build some very exciting projects
---
+++ Day-1/input.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+now = datetime.now()
+current_time = int(now.strftime("%H"))
+
+if(current_time < 12):
+    greet = "Morning"
+elif(current_time<16):
+    greet = "Afternoon"
+elif(current_time<20):
+    greet = "Evening"
+else:
+    greet = "Night"
+
+name = input("What's ur name? ")
+# print(current_time)
+print(f"Good {greet} {name}!!")
+
+print(len(name)) #length of the string
