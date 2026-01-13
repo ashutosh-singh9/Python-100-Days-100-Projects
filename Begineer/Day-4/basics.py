f96d61f6f42b96f107444a95f30e5b2f1703b9a1
Ashutosh Singh <singhashutosh4123@gmail.com>
2026-01-14 02:15:29 +0530

    restructured files

---
+++ Begineer/Day-4/basics.py
@@ -0,0 +1,31 @@
+# ----------whats a module? Built in & user defined modules
+
+# import module
+# print(module.sum(5,6))
+
+# calculator:
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# choose = int(input("1 for addition\n2 for substraction\n3 for multiplication\n4 for division\n"))
+
+# if choose == 1:
+#     print (f"your answer is {module.sum(a,b)}")
+# elif choose == 2:
+#     print (f"your answer is {module.sub(a,b)}")
+# elif choose == 3:
+#     print (f"your answer is {module.mul(a,b)}")
+# elif choose == 4:
+#     print (f"your answer is {module.div(a,b)}")
+# else:
+#     print("Invalid Options")
+
+
+# --------------random module
+
+import random
+import math
+
+# num = random.random()*10
+# num2 = random.randint(0,1)
+# print(math.ceil(num))
+# print(num2)
