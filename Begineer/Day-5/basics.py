6e937c98af9138df53f4831610975aab0cce1ca8
Ashutosh Singh <singhashutosh4123@gmail.com>
2026-01-15 19:40:23 +0530

    miniproject added

---
+++ Begineer/Day-5/basics.py
@@ -0,0 +1,2 @@
+# loop in list
+# py is number friendly language it has a lot of funtions supporting mathematical operations in list,etc
---
+++ Begineer/Day-5/miniproject.py
@@ -0,0 +1,32 @@
+marks = [
+    178, 152, 134, 190, 165,
+    147, 120, 198, 176, 154,
+    132, 188, 143, 157, 169,
+    121, 110, 195, 182, 140,
+    158, 172, 136, 149, 160,
+    101, 125, 185, 199, 167
+]
+
+
+#Add all
+
+# Method 1 (ez)
+# print(sum(marks))
+
+#method 2 (fundamental)
+# sum = 0
+# for mark in marks:
+#     sum+=mark
+# print(sum)
+
+# find the max no of all
+
+# method 1 (ez)
+# print(max(marks))
+
+#method 2 (fundamental)
+max_val = 0
+for mark in marks:   
+    if max_val < mark:
+        max_val = mark
+print(max_val)
