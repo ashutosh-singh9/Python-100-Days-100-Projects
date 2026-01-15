6fb8edc39dccfcf9d0b50b7162fb363255e88f78
Ashutosh Singh <singhashutosh4123@gmail.com>
2026-01-15 23:14:01 +0530

    project added

--- Begineer/Day-5/basics.py
+++ Begineer/Day-5/basics.py
@@ -1,2 +1,5 @@
 # loop in list
 # py is number friendly language it has a lot of funtions supporting mathematical operations in list,etc
+# loop with range()
+#range(a,b) run from a to b-1
+#range(a,b,c) run from a to b-1 and just c steps
--- Begineer/Day-5/miniproject.py
+++ Begineer/Day-5/miniproject.py
@@ -1,11 +1,13 @@
-marks = [
-    178, 152, 134, 190, 165,
-    147, 120, 198, 176, 154,
-    132, 188, 143, 157, 169,
-    121, 110, 195, 182, 140,
-    158, 172, 136, 149, 160,
-    101, 125, 185, 199, 167
-]
+# MiniProject 1
+
+# marks = [
+#     178, 152, 134, 190, 165,
+#     147, 120, 198, 176, 154,
+#     132, 188, 143, 157, 169,
+#     121, 110, 195, 182, 140,
+#     158, 172, 136, 149, 160,
+#     101, 125, 185, 199, 167
+# ]
 
 
 #Add all
@@ -25,8 +27,17 @@ marks = [
 # print(max(marks))
 
 #method 2 (fundamental)
-max_val = 0
-for mark in marks:   
-    if max_val < mark:
-        max_val = mark
-print(max_val)
+# max_val = 0
+# for mark in marks:   
+#     if max_val < mark:
+#         max_val = mark
+# print(max_val)
+
+# miniproject 2
+# sum till 100
+
+# sum = 0
+# for i in range(1,101):
+#     sum +=i
+    
+# print(sum)
