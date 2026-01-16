9fa3132d54667494a1d012089a7e40befb976dbb
Ashutosh Singh <singhashutosh4123@gmail.com>
2026-01-17 00:09:56 +0530

    hard method done

--- Begineer/Day-5/project.py
+++ Begineer/Day-5/project.py
@@ -2,7 +2,10 @@ import random
 letters = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x', 'y', 'z'
+    'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
 
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -42,3 +45,27 @@ nr_numbers = int(input("How many numbers would you like?\n"))
 # for i in range(nr_numbers):
 #     password += random.choice(numbers)
 # print(password)
+
+
+
+# hard _v1
+# a = []
+# for _ in range(nr_letters):
+#     a.append(random.choice(letters))
+
+
+# for _ in range(nr_symbols):
+#     a.append(random.choice(symbols))
+
+
+# for _ in range(nr_numbers):
+#     a.append(random.choice(numbers))
+    
+# random.shuffle(a)
+
+# gen_pass = ""
+# for i in a:
+#     gen_pass += i
+# print(gen_pass)
+
+# _v2(mam's approach)
