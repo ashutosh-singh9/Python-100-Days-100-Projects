8d9f978ff7f9786d44a9b2b18021b1d3a021adcb
Ashutosh Singh <singhashutosh4123@gmail.com>
2026-01-20 20:56:35 +0530

    Day content updated

---
+++ Begineer/Day-8/basics.py
@@ -0,0 +1,22 @@
+# funtion-> parameter argument
+
+# def funtion(parameter):
+#     //Do something
+
+# funtion(argument)
+
+
+#positional argument vs keyword arguement
+# 
+# def funtion (a,b,c):
+    # print(a)
+    # print(b)
+    # print(c)
+# 
+# funtion(5,4,3) #positional argument
+# 
+# //output-> 5,4,3
+
+# funtion(a=2,c=5,b=3) #keyword argument
+
+# //output-> 2,3,5
